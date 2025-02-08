fb559f3fe0999db3a965ed0ae629875320a54841
kalp9197 <pkalp61@gmail.com>
2025-02-08 16:51:09 +0530

    Added Search Functionality , WatchPage trailer view on yt

---
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ChevronRight } from "lucide-react";
+
+const AuthScreen = () => {
+	const [email, setEmail] = useState("");
+	const navigate = useNavigate();
+
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		navigate("/signup?email=" + email);
+	};
+
+	return (
+		<div className='hero-bg relative'>
+			{/* Navbar */}
+			<header className='max-w-6xl mx-auto flex items-center justify-between p-4 pb-10'>
+				<h1 className="text-3xl pb-2 text-white">NovaStream</h1>
+				<Link to={"/login"} className='text-white bg-violet-600 py-1 px-2 rounded'>
+					Sign In
+				</Link>
+			</header>
+
+			{/* Hero section */}
+			<div className='flex flex-col items-center justify-center text-center py-40 text-white max-w-6xl mx-auto'>
+				<h1 className='text-4xl md:text-6xl font-bold mb-4'>Unlimited movies, TV shows, and more</h1>
+				<p className='mb-4'>Ready to watch? Enter your email to create your account</p>
+
+				<form className='flex flex-col md:flex-row gap-4 w-1/2' onSubmit={handleFormSubmit}>
+					<input
+						type='email'
+						placeholder='Email address'
+						className='p-2 rounded flex-1 bg-black/80 border border-gray-700'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<button className='bg-violet-600 text-xl lg:text-2xl px-2 lg:px-6 py-1 md:py-2 rounded flex justify-center items-center'>
+						Get Started
+						<ChevronRight className='size-8 md:size-10' />
+					</button>
+				</form>
+			</div>
+
+			{/* Separator */}
+			<div className='h-2 w-full bg-[#232323]' aria-hidden='true' />
+
+			{/* Watch Everywhere Section */}
+			<div className='py-10 bg-black text-white'>
+				<div className='flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2'>
+					{/* Left side */}
+					<div className='flex-1 text-center md:text-left'>
+						<h2 className='text-4xl md:text-5xl font-extrabold mb-4'>Watch everywhere</h2>
+						<p className='text-lg md:text-xl'>
+							Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.
+						</p>
+					</div>
+
+					{/* Right side */}
+					<div className='flex-1 relative overflow-hidden'>
+						<img src='/device-pile.png' alt='Device image' className='mt-4 z-20 relative' />
+						<video
+							className='absolute top-2 left-1/2 -translate-x-1/2 h-4/6 z-10 max-w-[63%]'
+							playsInline
+							autoPlay
+							muted
+							loop
+						>
+							<source src='/video-devices.m4v' type='video/mp4' />
+						</video>
+					</div>
+				</div>
+			</div>
+
+			{/* Separator */}
+			<div className='h-2 w-full bg-[#232323]' aria-hidden='true' />
+		</div>
+	);
+};
+
+export default AuthScreen;
